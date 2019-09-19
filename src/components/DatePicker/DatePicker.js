74f125411ca45247a3d44971a487bad234ef0bf2
Stas Bashilov <stasbashilov@gmail.com>
2019-09-19 09:37:01 +0300

    F0003_update_datepicker

--- src/components/DatePicker/DatePicker.js
+++ src/components/DatePicker/DatePicker.js
@@ -1,30 +1,56 @@
 import React from "react";
-import DatePicker from "react-datepicker";
- 
-import "react-datepicker/dist/react-datepicker.css";
+import { DateRange } from 'react-date-range';
+
 import './DatePicker.css';
- 
-// CSS Modules, react-datepicker-cssmodules.css
-// import 'react-datepicker/dist/react-datepicker-cssmodules.css';
- 
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+
 class CustomDatePicker extends React.Component {
   state = {
-    startDate: new Date()
-  };
- 
-  handleChange = date => {
+    isVisible: false,
+    selectionRange: {
+      startDate: new Date(),
+      endDate: new Date(),
+      color: '#0FB2AB',
+      key: 'selection',
+    }
+  }
+
+  handleSelect = (ranges) => {
     this.setState({
-      startDate: date
-    });
-  };
- 
+
+      selectionRange: {
+        ...this.state.selectionRange,
+        startDate: ranges.selection.startDate,
+        endDate: ranges.selection.endDate
+      }
+    })
+  }
+
+  handlePickerShow = () => this.setState({ isVisible: true })
+  handlePickerClose = () => this.setState({ isVisible: false })
+
   render() {
+    const className = `${this.state.isVisible ? 'datepicker_close datepicker_close--rotate' : 'datepicker_close'} `;
+
     return (
-      <DatePicker
-        selected={this.state.startDate}
-        onChange={this.handleChange}
-      />
-    );
+      <div className='container'>
+        <div className={className} onClick={this.state.isVisible ? this.handlePickerClose : this.handlePickerShow}></div>
+        
+        <div className='react-datepicker__input-container' onClick={this.handlePickerShow}>
+          {this.state.selectionRange.startDate.toLocaleDateString()}
+          {' - '}
+          {this.state.selectionRange.endDate.toLocaleDateString()}
+        </div>
+       
+        {this.state.isVisible ? <div className='datepicker'>
+            <DateRange
+              ranges={[this.state.selectionRange]}
+              onChange={this.handleSelect}
+            />
+          </div> : null}
+      </div>
+    )
   }
 }
 
