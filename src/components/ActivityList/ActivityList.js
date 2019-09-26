8d0099cb503da6a4a994a4700f275bb3aa6e7b00
Stas Bashilov <stasbashilov@gmail.com>
2019-09-26 15:43:46 +0300

    F0006_fixed_scroll_activity_list

--- src/components/ActivityList/ActivityList.js
+++ src/components/ActivityList/ActivityList.js
@@ -25,29 +25,37 @@ class ActivityList extends React.Component {
 
         return (
             <div className='activity-list'>
+
+
                 <div className='activity-list__header'>Activity</div>
-                {this.state.activities.map((item, index) => {
-                    return (
-                        <div key={index} className='activity-list__item'>
-                            <div className='item__img'>{images.find(img => img.type === item.type).img()}</div>
-                            <div className='activity-list__name'>
-                                <div className='activity-list__item__name'>{item.name}</div>
-                                <div>
-                                {item.complited ?
-                                    <span className='item__complited'>Complited</span>
-                                    :
-                                    <span className='item__pending'>Pending</span>
-                                }
-                                <span className='activity-list__item__date'>{item.date}</span>
+
+                <div className='activity-list__body'>
+                    {this.state.activities.map((item, index) => {
+                        return (
+                            <div key={index} className='activity-list__item'>
+                                <div className='item__img'>{images.find(img => img.type === item.type).img()}</div>
+                                <div className='activity-list__name'>
+                                    <div className='activity-list__item__name'>{item.name}</div>
+                                    <div>
+                                    {item.complited ?
+                                        <span className='item__complited'>Complited</span>
+                                        :
+                                        <span className='item__pending'>Pending</span>
+                                    }
+                                    <span className='activity-list__item__date'>{item.date}</span>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className='activity-list__price'>
-                                <div className='activity-list__item__bill'>{item.price}</div>
-                                <div className='activity-list__item__type'>{item.type}</div>
-                            </div>
-                        </div>)
-                })}
-                <div className='activity-list__button' onClick={this.handleGetItems}>Load more</div>
+                                <div className='activity-list__price'>
+                                    <div className='activity-list__item__bill'>{item.price}</div>
+                                    <div className='activity-list__item__type'>{item.type}</div>
+                                </div>
+                            </div>)
+                    })}
+                </div>
+
+                <div className='activity-list__footer'>
+                    <div className='activity-list__button' onClick={this.handleGetItems}>Load more</div>
+                </div>
             </div>
         )
     }
